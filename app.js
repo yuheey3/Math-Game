14226af17bc6cc4b64d34c90156511966937953b
Yuki Waka <ywaka@myseneca.ca>
2020-07-10 01:45:27 -0400

    commit3

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 
 // Redirect Users to "index.html" if route not accessed using client side routing
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname + "/Public/index.html"));
+    res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 
