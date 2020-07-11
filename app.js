cc0cc1e9e80a68e73f26959ddf2aae7b621ebf43
Yuki Waka <ywaka@myseneca.ca>
2020-07-11 00:05:42 -0400

    commit1ssssdddd

--- app.js
+++ app.js
@@ -6,16 +6,11 @@ var app = express();
 var HTTP_PORT = process.env.PORT || 8080
 
 app.use(express.static("public"));
-app.use(express.static("HTML"));
-app.use(express.static("CSS"));
-app.use(express.static("JavaScript"));
 
 // Redirect Users to "index.html" if route not accessed using client side routing
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/public/index.html"));
 });
-
-
 
 app.listen(HTTP_PORT, ()=>{
     console.log("listening on port: " + HTTP_PORT);
